fa345690e2334884c15ed4119384aba8ccb6db95
Ryan Ray <ryan.ray.bio@gmail.com>
2026-05-13 14:09:15 -0600

    Update Main.py

--- Main.py
+++ Main.py
@@ -17,9 +17,7 @@ st.title("SSIG Field Survey Assistant")
 # OPENAI CLIENT
 # =========================
 
-client = OpenAI(
-    api_key=st.secrets["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"]
-)
+api_key=st.secrets["OPENAI_API_KEY"]
 
 # =========================
 # LOAD SPREADSHEET
